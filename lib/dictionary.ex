0f36dba17820fdf1b23f336750958b60482b7607
Bulat Gombotsyrenov <bulawake@gmail.com>
2019-11-21 21:27:53 +0700

    refactor to pipelines

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,10 +1,12 @@
 defmodule Dictionary do
   def word_list do
-    contents = File.read!("assets/words.txt")
-    list      = String.split(contents, "\n")
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split("\n")
   end
 
   def random_word() do
-    Enum.random(word_list())
+    word_list()
+    |> Enum.random()
   end
 end
