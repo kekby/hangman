afa92c12f68f0c0ca9778de0c5e871c3c96f4587
Bulat Gombotsyrenov <bulawake@gmail.com>
2019-12-24 22:56:50 +0700

    playing with view

--- gallows/lib/gallows_web/views/page_view.ex
+++ gallows/lib/gallows_web/views/page_view.ex
@@ -1,7 +1,10 @@
 defmodule GallowsWeb.PageView do
   use GallowsWeb, :view
 
-  def plural_of(word, 1), do: word
+  def plural_of(word, 1), do: "#{1} #{word}"
+  def plural_of(word, count) when count < 0 do
+    ~e"<span style='color: red'><%= count %> <%= word %>s</span>"
+  end
 
-  def plural_of(word, _), do: word <> "s"
+  def plural_of(word, count), do: "#{count} #{word}s"
 end
